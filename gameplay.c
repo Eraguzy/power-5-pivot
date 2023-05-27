1ca49b5d156dc20b4ee269cefb643a8feb0acd7c
eraguzy <chekhab.elias@gmail.com>
2023-05-27 18:24:56 +0200

    confirm before pivot
    - now shows what pivot does before applying gravity

--- gameplay.c
+++ gameplay.c
@@ -62,175 +62,188 @@ int pivotBoard(int lines, int columns, char** tab) {
     int pivotRow, pivotColumn; //rows and columns index
     int emptycheck=0;
     char pivotDirection; //left or right
+    char confirm;
 
-    printf("\nTime to rotate !\nChoose the size of the pivot (3 or 5):\n");
     do{
-        checkscanf = scanf(" %d", &pivotSize);
-        emptybuffer();
-        if(checkscanf != 1 || (pivotSize != 3 && pivotSize != 5)) { //this part avoids bugs from scanf inputs
-            printf("Please choose a valid entry ('3' or '5').\n"); 
-        }
-    } while(checkscanf != 1 || (pivotSize != 3 && pivotSize != 5));
+        printf("\nTime to rotate !\nChoose the size of the pivot (3 or 5):\n");
+        do{
+            checkscanf = scanf(" %d", &pivotSize);
+            emptybuffer();
+            if(checkscanf != 1 || (pivotSize != 3 && pivotSize != 5)) { //this part avoids bugs from scanf inputs
+                printf("Please choose a valid entry ('3' or '5').\n"); 
+            }
+        } while(checkscanf != 1 || (pivotSize != 3 && pivotSize != 5));
 
 
-    do { //pivot selection
-        printf("\nChoose the row index (↓) of the pivot : rotation cannot be outside the game (%d-%d) and the square cannot be empty \n", (pivotSize/2)+1, lines-(pivotSize/2));
-        checkscanf = scanf("%d", &pivotRow);
-        emptybuffer();
+        do { //pivot selection
+            printf("\nChoose the row index (↓) of the pivot : rotation cannot be outside the game (%d-%d) and the square cannot be empty \n", (pivotSize/2)+1, lines-(pivotSize/2));
+            checkscanf = scanf("%d", &pivotRow);
+            emptybuffer();
 
-        printf("Choose the column (→) index of the pivot : rotation cannot be outside the game (%d-%d) and the square cannot be empty \n", (pivotSize/2)+1, columns-(pivotSize/2));
-        checkscanf2 = scanf("%d", &pivotColumn);
-        emptybuffer();
+            printf("Choose the column (→) index of the pivot : rotation cannot be outside the game (%d-%d) and the square cannot be empty \n", (pivotSize/2)+1, columns-(pivotSize/2));
+            checkscanf2 = scanf("%d", &pivotColumn);
+            emptybuffer();
 
-        if(pivotRow < (pivotSize/2)+1 || pivotRow > lines-(pivotSize/2)) { //this part avoids bugs from scanf inputs
-            printf("error : Please choose a valid row entry (between %d and %d).\n",(pivotSize/2)+1, lines-(pivotSize/2));  //check for row
-        }
-        if(pivotColumn < (pivotSize/2)+1 || pivotColumn > columns-(pivotSize/2)) { //this part avoids bugs from scanf inputs
-            printf("error : Please choose a valid column entry (between %d and %d).\n",(pivotSize/2)+1, columns-(pivotSize/2));    //check for column
-        }
-        if(checkscanf!=1 || checkscanf2!=1){ 
-            printf("please stop typing stuff like this it won't work\n");
-        }
+            if(pivotRow < (pivotSize/2)+1 || pivotRow > lines-(pivotSize/2)) { //this part avoids bugs from scanf inputs
+                printf("error : Please choose a valid row entry (between %d and %d).\n",(pivotSize/2)+1, lines-(pivotSize/2));  //check for row
+            }
+            if(pivotColumn < (pivotSize/2)+1 || pivotColumn > columns-(pivotSize/2)) { //this part avoids bugs from scanf inputs
+                printf("error : Please choose a valid column entry (between %d and %d).\n",(pivotSize/2)+1, columns-(pivotSize/2));    //check for column
+            }
+            if(checkscanf!=1 || checkscanf2!=1){ 
+                printf("please stop typing stuff like this it won't work\n");
+            }
 
-        if((pivotRow >= (pivotSize/2)+1 && pivotRow <= lines-(pivotSize/2)) && (pivotColumn >= (pivotSize/2)+1 && pivotColumn <= columns-(pivotSize/2))){// avoids seg faults from the emptycheck
-            for (int k = 0, i=pivotRow-(pivotSize/2)-1; k < pivotSize; k++, i++) { //check if empty : k and l makes the array test clearer
-                for (int l = 0, j=pivotColumn-(pivotSize/2)-1; l < pivotSize; l++, j++) { //-1 for i and j bc of the decrementation (applied later)
-                    if(tab[i][j]!=' '){
-                        emptycheck=1;
+            if((pivotRow >= (pivotSize/2)+1 && pivotRow <= lines-(pivotSize/2)) && (pivotColumn >= (pivotSize/2)+1 && pivotColumn <= columns-(pivotSize/2))){// avoids seg faults from the emptycheck
+                for (int k = 0, i=pivotRow-(pivotSize/2)-1; k < pivotSize; k++, i++) { //check if empty : k and l makes the array test clearer
+                    for (int l = 0, j=pivotColumn-(pivotSize/2)-1; l < pivotSize; l++, j++) { //-1 for i and j bc of the decrementation (applied later)
+                        if(tab[i][j]!=' '){
+                            emptycheck=1;
+                        }
                     }
                 }
+                if(emptycheck!=1){
+                    printf("error : you can't choose an empty square\n");
+                }
             }
-            if(emptycheck!=1){
-                printf("error : you can't choose an empty square\n");
+        } while(emptycheck!=1 || (checkscanf!=1 || checkscanf2 != 1) || (pivotRow < (pivotSize/2)+1 || pivotRow > lines-(pivotSize/2) || (pivotColumn < (pivotSize/2)+1 || pivotColumn > columns-(pivotSize/2))));
+        pivotRow--;
+        pivotColumn--;
+
+
+        printf("Choose the direction :\nr: right (clockwise)          l: left (anticlockwise)\n");
+        do {
+            checkscanf = scanf(" %c", &pivotDirection);
+            emptybuffer();
+            if(checkscanf != 1 || (pivotDirection != 'r' && pivotDirection != 'l')) { //this part avoids bugs from scanf inputs
+                printf("Please choose a valid entry ('r' or 'l').\n"); 
             }
-        }
-    } while(emptycheck!=1 || (checkscanf!=1 || checkscanf2 != 1) || (pivotRow < (pivotSize/2)+1 || pivotRow > lines-(pivotSize/2) || (pivotColumn < (pivotSize/2)+1 || pivotColumn > columns-(pivotSize/2))));
-    pivotRow--;
-    pivotColumn--;
-
-
-    printf("Choose the direction :\nr: right (clockwise)          l: left (anticlockwise)\n");
-    do {
-        checkscanf = scanf(" %c", &pivotDirection);
-        emptybuffer();
-        if(checkscanf != 1 || (pivotDirection != 'r' && pivotDirection != 'l')) { //this part avoids bugs from scanf inputs
-            printf("Please choose a valid entry ('r' or 'l').\n"); 
-        }
-    } while(checkscanf != 1 || (pivotDirection != 'r' && pivotDirection != 'l'));
+        } while(checkscanf != 1 || (pivotDirection != 'r' && pivotDirection != 'l'));
 
 
 
-    char** buffer = malloc(sizeof(char*) * pivotSize); //malloc for buffer tab
-    if (buffer == NULL) {
-        printf("malloc error\n");
-        exit(0);
-    }
-    for (int j = 0; j < pivotSize; j++) {
-        buffer[j] = malloc(sizeof(char) * pivotSize);
-        if (buffer[j] == NULL) {
+        char** buffer = malloc(sizeof(char*) * pivotSize); //malloc for buffer tab
+        if (buffer == NULL) {
             printf("malloc error\n");
             exit(0);
         }
-    }
-
-    for (int k = 0, i=pivotRow-(pivotSize/2); k < pivotSize; k++, i++) {
-        for (int l = 0, j=pivotColumn-(pivotSize/2); l < pivotSize; l++, j++) {
-            buffer[k][l] = tab[i][j]; // copy to the buffer
+        for (int j = 0; j < pivotSize; j++) {
+            buffer[j] = malloc(sizeof(char) * pivotSize);
+            if (buffer[j] == NULL) {
+                printf("malloc error\n");
+                exit(0);
+            }
         }
-    }
 
-
-
-    if(pivotSize==3){
-        if(pivotDirection=='r'){
-            tab[pivotRow-1][pivotColumn-1]=buffer[2][0];
-            tab[pivotRow-1][pivotColumn]=buffer[1][0];
-            tab[pivotRow-1][pivotColumn+1]=buffer[0][0];
-
-            tab[pivotRow][pivotColumn-1]=buffer[2][1];
-            tab[pivotRow][pivotColumn+1]=buffer[0][1];
-
-            tab[pivotRow+1][pivotColumn-1]=buffer[2][2];
-            tab[pivotRow+1][pivotColumn]=buffer[1][2];
-            tab[pivotRow+1][pivotColumn+1]=buffer[0][2];
+        for (int k = 0, i=pivotRow-(pivotSize/2); k < pivotSize; k++, i++) {
+            for (int l = 0, j=pivotColumn-(pivotSize/2); l < pivotSize; l++, j++) {
+                buffer[k][l] = tab[i][j]; // copy to the buffer
+            }
         }
-        else if(pivotDirection=='l'){
-            tab[pivotRow-1][pivotColumn-1] = buffer[0][2];
-            tab[pivotRow-1][pivotColumn]   = buffer[1][2];
-            tab[pivotRow-1][pivotColumn+1] = buffer[2][2];
 
-            tab[pivotRow][pivotColumn-1]   = buffer[0][1];
-            tab[pivotRow][pivotColumn]     = buffer[1][1];
-            tab[pivotRow][pivotColumn+1]   = buffer[2][1];
 
-            tab[pivotRow+1][pivotColumn-1] = buffer[0][0];
-            tab[pivotRow+1][pivotColumn]   = buffer[1][0];
-            tab[pivotRow+1][pivotColumn+1] = buffer[2][0];
+
+        if(pivotSize==3){
+            if(pivotDirection=='r'){
+                tab[pivotRow-1][pivotColumn-1]=buffer[2][0];
+                tab[pivotRow-1][pivotColumn]=buffer[1][0];
+                tab[pivotRow-1][pivotColumn+1]=buffer[0][0];
+
+                tab[pivotRow][pivotColumn-1]=buffer[2][1];
+                tab[pivotRow][pivotColumn+1]=buffer[0][1];
+
+                tab[pivotRow+1][pivotColumn-1]=buffer[2][2];
+                tab[pivotRow+1][pivotColumn]=buffer[1][2];
+                tab[pivotRow+1][pivotColumn+1]=buffer[0][2];
+            }
+            else if(pivotDirection=='l'){
+                tab[pivotRow-1][pivotColumn-1] = buffer[0][2];
+                tab[pivotRow-1][pivotColumn]   = buffer[1][2];
+                tab[pivotRow-1][pivotColumn+1] = buffer[2][2];
+
+                tab[pivotRow][pivotColumn-1]   = buffer[0][1];
+                tab[pivotRow][pivotColumn]     = buffer[1][1];
+                tab[pivotRow][pivotColumn+1]   = buffer[2][1];
+
+                tab[pivotRow+1][pivotColumn-1] = buffer[0][0];
+                tab[pivotRow+1][pivotColumn]   = buffer[1][0];
+                tab[pivotRow+1][pivotColumn+1] = buffer[2][0];
+            }
         }
-    }
-    else if(pivotSize==5){
-        if(pivotDirection=='r'){
-            tab[pivotRow-2][pivotColumn-2] = buffer[4][0];
-            tab[pivotRow-2][pivotColumn-1] = buffer[3][0];
-            tab[pivotRow-2][pivotColumn] = buffer[2][0];
-            tab[pivotRow-2][pivotColumn+1] = buffer[1][0];
-            tab[pivotRow-2][pivotColumn+2] = buffer[0][0];
+        else if(pivotSize==5){
+            if(pivotDirection=='r'){
+                tab[pivotRow-2][pivotColumn-2] = buffer[4][0];
+                tab[pivotRow-2][pivotColumn-1] = buffer[3][0];
+                tab[pivotRow-2][pivotColumn] = buffer[2][0];
+                tab[pivotRow-2][pivotColumn+1] = buffer[1][0];
+                tab[pivotRow-2][pivotColumn+2] = buffer[0][0];
 
-            tab[pivotRow-1][pivotColumn-2] = buffer[4][1];
-            tab[pivotRow-1][pivotColumn-1] = buffer[3][1];
-            tab[pivotRow-1][pivotColumn] = buffer[2][1];
-            tab[pivotRow-1][pivotColumn+1] = buffer[1][1];
-            tab[pivotRow-1][pivotColumn+2] = buffer[0][1];
+                tab[pivotRow-1][pivotColumn-2] = buffer[4][1];
+                tab[pivotRow-1][pivotColumn-1] = buffer[3][1];
+                tab[pivotRow-1][pivotColumn] = buffer[2][1];
+                tab[pivotRow-1][pivotColumn+1] = buffer[1][1];
+                tab[pivotRow-1][pivotColumn+2] = buffer[0][1];
 
-            tab[pivotRow][pivotColumn-2] = buffer[4][2];
-            tab[pivotRow][pivotColumn-1] = buffer[3][2];
-            tab[pivotRow][pivotColumn+1] = buffer[1][2];
-            tab[pivotRow][pivotColumn+2] = buffer[0][2];
+                tab[pivotRow][pivotColumn-2] = buffer[4][2];
+                tab[pivotRow][pivotColumn-1] = buffer[3][2];
+                tab[pivotRow][pivotColumn+1] = buffer[1][2];
+                tab[pivotRow][pivotColumn+2] = buffer[0][2];
 
-            tab[pivotRow+1][pivotColumn-2] = buffer[4][3];
-            tab[pivotRow+1][pivotColumn-1] = buffer[3][3];
-            tab[pivotRow+1][pivotColumn] = buffer[2][3];
-            tab[pivotRow+1][pivotColumn+1] = buffer[1][3];
-            tab[pivotRow+1][pivotColumn+2] = buffer[0][3];
+                tab[pivotRow+1][pivotColumn-2] = buffer[4][3];
+                tab[pivotRow+1][pivotColumn-1] = buffer[3][3];
+                tab[pivotRow+1][pivotColumn] = buffer[2][3];
+                tab[pivotRow+1][pivotColumn+1] = buffer[1][3];
+                tab[pivotRow+1][pivotColumn+2] = buffer[0][3];
 
-            tab[pivotRow+2][pivotColumn-2] = buffer[4][4];
-            tab[pivotRow+2][pivotColumn-1] = buffer[3][4];
-            tab[pivotRow+2][pivotColumn] = buffer[2][4];
-            tab[pivotRow+2][pivotColumn+1] = buffer[1][4];
-            tab[pivotRow+2][pivotColumn+2] = buffer[0][4];
+                tab[pivotRow+2][pivotColumn-2] = buffer[4][4];
+                tab[pivotRow+2][pivotColumn-1] = buffer[3][4];
+                tab[pivotRow+2][pivotColumn] = buffer[2][4];
+                tab[pivotRow+2][pivotColumn+1] = buffer[1][4];
+                tab[pivotRow+2][pivotColumn+2] = buffer[0][4];
+            }
+            else if(pivotDirection=='l'){
+                tab[pivotRow-2][pivotColumn-2] = buffer[0][4];
+                tab[pivotRow-2][pivotColumn-1] = buffer[1][4];
+                tab[pivotRow-2][pivotColumn] = buffer[2][4];
+                tab[pivotRow-2][pivotColumn+1] = buffer[3][4];
+                tab[pivotRow-2][pivotColumn+2] = buffer[4][4];
+
+                tab[pivotRow-1][pivotColumn-2] = buffer[0][3];
+                tab[pivotRow-1][pivotColumn-1] = buffer[1][3];
+                tab[pivotRow-1][pivotColumn] = buffer[2][3];
+                tab[pivotRow-1][pivotColumn+1] = buffer[3][3];
+                tab[pivotRow-1][pivotColumn+2] = buffer[4][3];
+
+                tab[pivotRow][pivotColumn-2] = buffer[0][2];
+                tab[pivotRow][pivotColumn-1] = buffer[1][2];
+                tab[pivotRow][pivotColumn+1] = buffer[3][2];
+                tab[pivotRow][pivotColumn+2] = buffer[4][2];
+
+                tab[pivotRow+1][pivotColumn-2] = buffer[0][1];
+                tab[pivotRow+1][pivotColumn-1] = buffer[1][1];
+                tab[pivotRow+1][pivotColumn] = buffer[2][1];
+                tab[pivotRow+1][pivotColumn+1] = buffer[3][1];
+                tab[pivotRow+1][pivotColumn+2] = buffer[4][1];
+
+                tab[pivotRow+2][pivotColumn-2] = buffer[0][0];
+                tab[pivotRow+2][pivotColumn-1] = buffer[1][0];
+                tab[pivotRow+2][pivotColumn] = buffer[2][0];
+                tab[pivotRow+2][pivotColumn+1] = buffer[3][0];
+                tab[pivotRow+2][pivotColumn+2] = buffer[4][0];  
+            }
         }
-        else if(pivotDirection=='l'){
-            tab[pivotRow-2][pivotColumn-2] = buffer[0][4];
-            tab[pivotRow-2][pivotColumn-1] = buffer[1][4];
-            tab[pivotRow-2][pivotColumn] = buffer[2][4];
-            tab[pivotRow-2][pivotColumn+1] = buffer[3][4];
-            tab[pivotRow-2][pivotColumn+2] = buffer[4][4];
-
-            tab[pivotRow-1][pivotColumn-2] = buffer[0][3];
-            tab[pivotRow-1][pivotColumn-1] = buffer[1][3];
-            tab[pivotRow-1][pivotColumn] = buffer[2][3];
-            tab[pivotRow-1][pivotColumn+1] = buffer[3][3];
-            tab[pivotRow-1][pivotColumn+2] = buffer[4][3];
-
-            tab[pivotRow][pivotColumn-2] = buffer[0][2];
-            tab[pivotRow][pivotColumn-1] = buffer[1][2];
-            tab[pivotRow][pivotColumn+1] = buffer[3][2];
-            tab[pivotRow][pivotColumn+2] = buffer[4][2];
-
-            tab[pivotRow+1][pivotColumn-2] = buffer[0][1];
-            tab[pivotRow+1][pivotColumn-1] = buffer[1][1];
-            tab[pivotRow+1][pivotColumn] = buffer[2][1];
-            tab[pivotRow+1][pivotColumn+1] = buffer[3][1];
-            tab[pivotRow+1][pivotColumn+2] = buffer[4][1];
-
-            tab[pivotRow+2][pivotColumn-2] = buffer[0][0];
-            tab[pivotRow+2][pivotColumn-1] = buffer[1][0];
-            tab[pivotRow+2][pivotColumn] = buffer[2][0];
-            tab[pivotRow+2][pivotColumn+1] = buffer[3][0];
-            tab[pivotRow+2][pivotColumn+2] = buffer[4][0];  
-        }
-    }
+        displayer(lines, columns, tab); //displays the current state of the game
+        do{ // choice
+        printf("Apply this choice ?\ny:yes (apply)          n: no (set a new pivot)\n");
+            checkscanf=scanf(" %c", &confirm);
+            emptybuffer();
+            if(checkscanf!=1 || (confirm != 'y' && confirm != 'n')) { //this part avoids bugs from scanf inputs
+                printf("Please enter a valid entry ('y' or 'n').\n"); 
+            }
+        }while(checkscanf!=1 || (confirm != 'y' && confirm != 'n'));
+    }while (confirm!='y');
     return pivotColumn;
+}
 
 
     #if 0
@@ -302,7 +315,6 @@ int pivotBoard(int lines, int columns, char** tab) {
         }
         free(buffer);
     }
-
     return pivotSize;
-    #endif
 }
+    #endif
